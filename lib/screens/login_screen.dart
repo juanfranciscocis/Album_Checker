dbbfba6ff29bc675f165f6b5224656a4eb94499e
Juan Cisneros <juanfrancistm2011@icloud.com>
2022-09-04 14:47:27 -0500

    LOGIN SCREEN FINISHED DESIGN, ADAPTATIVE FOR GRAND AND SMALL DEVICES

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,63 +9,82 @@ class LoginScreen extends StatelessWidget{
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color.fromRGBO(190, 209, 121, 1),
-      body: Stack(
-        children: [
-          topCircle(size),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
 
-          Positioned(child: Text('WELCOME', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),), top: 100, left: 110,),
+              //SizedBox(height: size.height * 0.1),
+
+              Stack(
+                children: [
+                  topCircle(size),
+                  Padding(
+                    padding: const EdgeInsets.all(75),
+                    child: Center(child: Column(
+                      children: [
+                        Text('WELCOME TO THE ALBUM CHECKER APP', textAlign: TextAlign.justify,style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),),
+                        SizedBox(height: size.height * 0.05),
+                        Text('⚽',style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: Colors.white),),
+                      ],
+                    )),
+                  ),
+                ],
+              ),
 
 
-          _MainCardWidget(),
 
 
-          newToAppText(size, context)
 
-        ],
+
+              SizedBox(height: size.height * 0.1),
+
+
+              _MainCardWidget(),
+
+
+              newToAppText(size, context)
+
+            ],
+          ),
+        ),
       )
     );
   }
 
-  Positioned newToAppText(Size size, BuildContext context) {
-    return Positioned(
-            bottom: 0,
-            left: size.width *0.27,
-
-
-            child:GestureDetector(
-              onTap: (){
-                Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
-                    LoginScreen()), (Route<dynamic> route) => false);
-              },
-              child: Container(
-                width: size.width,
-                height: size.height * 0.3,
-                child: Text('New to the app? Register',textAlign: TextAlign.left, style: TextStyle(
-                color: Color.fromRGBO(255, 255, 255, 1),
-                fontWeight: FontWeight.bold,
-                fontSize: 15,
+  Widget newToAppText(Size size, BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+            LoginScreen()), (Route<dynamic> route) => false);
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: size.width,
+        height: size.height * 0.3,
+        child: Text('New to the app? Register',textAlign: TextAlign.left, style: TextStyle(
+        color: Color.fromRGBO(255, 255, 255, 1),
+        fontWeight: FontWeight.bold,
+        fontSize: 15,
         ),),
-              ),
-            ));
+      ),
+    );
   }
 
-  Positioned topCircle(Size size) {
-    return Positioned(
-          top: -225,
-          child: Container(
-              width: size.width,
-              height: 410,
-              decoration: const BoxDecoration(
-                boxShadow : [BoxShadow(
-                    color: Color.fromRGBO(0, 0, 0, 0.25),
-                    offset: Offset(0,4),
-                    blurRadius: 4
-                )],
-                color : Color.fromRGBO(105, 143, 63, 1),
-                borderRadius : BorderRadius.all(Radius.elliptical(414, 410)),
-              ),
-          ),
-        );
+  Widget topCircle(Size size) {
+    return Container(
+        width: size.width,
+        height: 400,
+        decoration: const BoxDecoration(
+          boxShadow : [BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.25),
+              offset: Offset(0,4),
+              blurRadius: 4
+          )],
+          color : Color.fromRGBO(105, 143, 63, 1),
+          borderRadius : BorderRadius.only(bottomLeft: Radius.elliptical(414, 410), bottomRight: Radius.elliptical(414, 410)),
+        ),
+    );
   }
 }
 
@@ -77,17 +96,14 @@ class _MainCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Positioned(
-      top: size.height * 0.3,
-      left: size.width * 0.09,
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
-        color: const Color.fromRGBO(20, 40, 29, 1),
-        child: Container(
-          width: size.width * 0.8,
-          height: size.height * 0.35,
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      color: const Color.fromRGBO(20, 40, 29, 1),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
           child: Column(
             children: [
               const SizedBox(
@@ -102,7 +118,7 @@ class _MainCardWidget extends StatelessWidget {
               const SizedBox(
                 height: 40,
               ),
-              
+
               MaterialButton(
                 child: Container(
                     width: 161,
@@ -131,9 +147,9 @@ class _MainCardWidget extends StatelessWidget {
 
 
             ]
-          )
+          ),
         )
-      ),
+      )
     );
   }
 }
